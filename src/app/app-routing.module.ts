cbb73cbd679feffac960feb5f19e04d8ea3946b2
hanenchemkhi <hanen.chemkhi@gmail.com>
2023-08-11 12:16:51 -0400

    Imported Bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './component/users/users.component';
+import { UserdetailComponent } from './component/userdetail/userdetail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'user/:uuid', component: UserdetailComponent},
+   //similar to { path: '**', component: UsersComponent}, '**' means if the user enter a path other than users or user
+  { path: '**', redirectTo: 'users'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { User } from '../interface/user.interface';
+import { Response } from '../interface/response.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly apiUrl: string = 'https://randomuser.me/api';
+
+  constructor(private http : HttpClient) { }
+
+  //fetch all users
+  getUsers(size: number = 10): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/?results=${size}`).pipe(
+      map(response => this.processResponse(response))
+    );
+  }
+
+  //fetch one user using the user UUID
+  getUser(uuid: number = 1): Observable<any>{
+    return this.http.get<any>('${this.apiUrl}/?uuid=${uuid}').pipe(
+      map(response => this.processResponse(response))
+    );
+  }
+
+  private processResponse (response: Response): Response{
+    return { 
+      info : { ...response.info }, //map everything from response.info to info object
+      results : response.results.map((user: any)=> (<User>{
+
+        uuid: user.login.uuid,
+        firstName: user.name.first,
+        lastName: user.name.last,
+        email: user.email,
+        username: user.login.username,
+        gender: user.gender, 
+        address: `${user.location.street.number} ${user.location.street.name} ${user.location.street.number} ${user.location.street.city}, ${user.location.street.country} ${user.location.street.postcode}  `,
+        dateOfBirth: user.dob.date,
+        phone: user.phone,
+        imageUrl: user.picture.medium,
+        coordinate: {
+          latitude : +user.location.coordinates.latitude, // to change a string to a number add + sign in front of the variable
+          longitude : +user.location.coordinates.longitude,
+        }
+      }))
+    }
+  }
+}
